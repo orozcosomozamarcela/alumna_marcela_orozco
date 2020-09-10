f4b9bcee2b37c94c10a3d832e27ac4231bb1d019
Marcela Orozco <orozcosomozamarcela@gmail.com>
2020-09-10 14:41:10 -0300

    estructura de datos primera prueba de git repo

---
+++ ejercicios_en_clases_estruc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios_en_clases_estruc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            
+            for (i=0; i<5; i++)
+            {
+            Console.WriteLine("Holis");
+            Console.ReadKey(); /*diferencia entre readkey y readline es que con el primero apretas una tecla y ya tomo lo que apretaste y con el segundo escribis y tenes que dar un enter para que termine.*/
+            }
+        }
+    }
+}
